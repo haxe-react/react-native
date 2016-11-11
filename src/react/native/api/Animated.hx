bc4ba938f3ef0ca445628606211fedd529e890b7
kevinresol <kevinresol@gmail.com>
2016-11-11 23:53:11 +0800

    Add components in Animated

--- src/react/native/api/Animated.hx
+++ src/react/native/api/Animated.hx
@@ -4,6 +4,9 @@ import js.Error;
 
 @:jsRequire('react-native', 'Animated')
 extern class Animated {
+	public static inline var View = react.native.component.AnimatedView;
+	public static inline var Text = react.native.component.AnimatedText;
+	public static inline var Image = react.native.component.AnimatedImage;
 	public function start():Void;
 	public static function timing(v:Dynamic, config:Dynamic):Animated;
 }
---
+++ src/react/native/component/AnimatedText.hx
@@ -0,0 +1,4 @@
+package react.native.component;
+
+@:jsRequire('react-native', 'Animated.Text')
+extern class AnimatedText {}
