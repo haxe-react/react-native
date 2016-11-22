1f5d071644fa02d438719f3277606b09ca45acc4
kevinresol <kevinresol@gmail.com>
2016-11-22 10:38:12 +0800

    Complete AsyncStorage API

--- src/react/native/api/AsyncStorage.hx
+++ src/react/native/api/AsyncStorage.hx
@@ -1,10 +1,18 @@
 package react.native.api;
 
 import js.Error;
+import js.Promise;
 
 @:jsRequire('react-native', 'AsyncStorage')
 extern class AsyncStorage {
-	public static function getItem(key:String, ?callback:Error->String->Void):Void;
-	public static function setItem(key:String, value:String, ?callback:Error->Void):Void;
-	public static function removeItem(key:String, ?callback:Error->Void):Void;
+	static function getItem(key:String, ?callback:Error->String->Void):Promise<String>;
+	static function setItem(key:String, value:String, ?callback:Error->Void):Promise<Void>;
+	static function removeItem(key:String, ?callback:Error->Void):Promise<Void>;
+	static function mergeItem(key:String, value:String, ?callback:Error->Void):Promise<Void>;
+	static function clear(?callback:Error->Array<String>->Void):Promise<Array<String>>;
+	static function flushGetRequests():Void;
+	static function multiGet(keys:Array<String>, ?callback:Array<Error>->Array<Array<String>>->Void):Promise<Array<Array<String>>>;
+	static function multiSet(keyValuePairs:Array<Array<String>>, ?callback:Array<Error>->Void):Promise<Void>;
+	static function multiRemove(keys:Array<String>, ?callback:Array<Error>->Void):Promise<Array<Array<String>>>;
+	static function multiMerge(keyValuePairs:Array<Array<String>>, ?callback:Array<Error>->Void):Promise<Void>;
 }
