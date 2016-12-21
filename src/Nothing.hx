8f57b9ded3a88d529624abbffdaed4884fabfd38
Kevin Leung <kevinresol@gmail.com>
2016-12-21 18:15:25 +0800

    Add Nothing helper

---
+++ src/Nothing.hx
@@ -0,0 +1,7 @@
+package;
+
+// TODO: this is so useful with js.Promise and should go into the std lib
+@:enum
+abstract Nothing(Dynamic) {
+	var Nil = null;
+}
--- src/react/native/api/AsyncStorage.hx
+++ src/react/native/api/AsyncStorage.hx
@@ -6,14 +6,14 @@ import js.Promise;
 @:jsRequire('react-native', 'AsyncStorage')
 extern class AsyncStorage {
 	static function getItem(key:String, ?callback:Error->String->Void):Promise<String>;
-	static function setItem(key:String, value:String, ?callback:Error->Void):Promise<Void>;
-	static function removeItem(key:String, ?callback:Error->Void):Promise<Void>;
-	static function mergeItem(key:String, value:String, ?callback:Error->Void):Promise<Void>;
+	static function setItem(key:String, value:String, ?callback:Error->Void):Promise<Nothing>;
+	static function removeItem(key:String, ?callback:Error->Void):Promise<Nothing>;
+	static function mergeItem(key:String, value:String, ?callback:Error->Void):Promise<Nothing>;
 	static function clear(?callback:Error->Array<String>->Void):Promise<Array<String>>;
 	static function flushGetRequests():Void;
 	static function getAllKeys(?callback:Error->Array<String>->Void):Promise<Array<String>>;
 	static function multiGet(keys:Array<String>, ?callback:Array<Error>->Array<Array<String>>->Void):Promise<Array<Array<String>>>;
-	static function multiSet(keyValuePairs:Array<Array<String>>, ?callback:Array<Error>->Void):Promise<Void>;
+	static function multiSet(keyValuePairs:Array<Array<String>>, ?callback:Array<Error>->Void):Promise<Nothing>;
 	static function multiRemove(keys:Array<String>, ?callback:Array<Error>->Void):Promise<Array<Array<String>>>;
-	static function multiMerge(keyValuePairs:Array<Array<String>>, ?callback:Array<Error>->Void):Promise<Void>;
+	static function multiMerge(keyValuePairs:Array<Array<String>>, ?callback:Array<Error>->Void):Promise<Nothing>;
 }
