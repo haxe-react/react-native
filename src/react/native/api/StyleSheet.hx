b326625d305fa4c806b2acedd64dc04f848c7817
Kevin Leung <kevinresol@gmail.com>
2016-12-21 11:49:26 +0800

    Type Stylesheet properly with macro

--- src/react/native/api/StyleSheet.hx
+++ src/react/native/api/StyleSheet.hx
@@ -1,6 +1,26 @@
 package react.native.api;
 
+import haxe.macro.Context;
+import haxe.macro.Expr;
+
 @:jsRequire("react-native", "StyleSheet")
 extern class StyleSheet {
-	static function create<T>(obj:T):T;
+	@:native('create') @:noCompletion static function _create<T>(obj:Dynamic):Dynamic;
+	
+	static inline macro function create(e:Expr):ExprOf<Dynmaic<Int>> {
+		switch Context.follow(Context.typeof(e)) {
+			case TAnonymous(_.get() => a):
+				var ct = ComplexType.TAnonymous([for(field in a.fields) {
+					access: [APublic],
+					kind: FVar(macro:Int, null),
+					meta: null,
+					name: field.name,
+					pos: field.pos
+				}]);
+				return macro (react.native.api.StyleSheet._create($e):$ct);
+			default:
+				Context.fatalError('Expected anonymous object', e.pos);
+				return macro null;
+		}
+	}
 }
