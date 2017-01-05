a4169f2c7fd0726ae207ced24f831d220bf34718
Kevin Leung <kevinresol@gmail.com>
2017-01-05 17:35:46 +0800

    Add DrawerLayoutAndroid members

--- src/react/native/component/DrawerLayoutAndroid.hx
+++ src/react/native/component/DrawerLayoutAndroid.hx
@@ -1,4 +1,14 @@
 package react.native.component;
 
 @:jsRequire('react-native', 'DrawerLayoutAndroid')
-extern class DrawerLayoutAndroid extends react.ReactComponent {}
+extern class DrawerLayoutAndroid extends react.ReactComponent {
+	static var positions:DrawerPosition;
+	function openDrawer():Void;
+	function closeDrawer():Void;
+}
+
+
+typedef DrawerPosition = {
+	Left:Dynamic,
+	Right:Dynamic,
+}
