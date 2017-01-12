e2a571340ed715155626434840afc06124a0cdc7
Kevin Leung <kevinresol@gmail.com>
2017-01-12 10:22:41 +0800

    Add Platform

---
+++ src/react/native/api/Platform.hx
@@ -0,0 +1,15 @@
+package react.native.api;
+
+@:jsRequire('react-native', 'Platform')
+extern class Platform {
+	static var OS:Os;
+	static var Version:Int;
+	static function select<A>(obj:Dynamic<A>):A;
+	static function isTVOS():Bool; // ios only
+}
+
+@:enum
+abstract Os(String) {
+	var IOS = 'ios';
+	var Android = 'android';
+}
