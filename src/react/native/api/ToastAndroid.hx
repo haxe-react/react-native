c61f860e870bd092a9ceb073b4a8325cf799273a
Kevin Leung <kevinresol@gmail.com>
2017-03-31 11:47:57 +0800

    Add ToastAndroid (closes #6)

---
+++ src/react/native/api/ToastAndroid.hx
@@ -0,0 +1,15 @@
+package react.native.api;
+
+@:jsRequire('react-native', 'ToastAndroid')
+extern class ToastAndroid {
+	static var SHORT:Duration;
+	static var LONG:Duration;
+	static var TOP:Gravity;
+	static var CENTER:Gravity;
+	static var BOTTOM:Gravity;
+	static function show(message:String, duration:Duration):Void;
+	static function showWithGravity(message:String, duration:Duration, gravity:Gravity):Void;
+}
+
+abstract Duration(String) {}
+abstract Gravity(String) {}
