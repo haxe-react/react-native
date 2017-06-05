ce5b36785e13d4b2ff6df40535b1861738fd6b7d
Kevin Leung <kevinresol@gmail.com>
2017-06-05 19:33:48 +0800

    More props

--- src/react/native/component/Switch.hx
+++ src/react/native/component/Switch.hx
@@ -1,4 +1,7 @@
 package react.native.component;
 
+import react.native.component.props.*;
+import react.ReactComponent;
+
 @:jsRequire('react-native', 'Switch')
-extern class Switch extends react.ReactComponent {}
+extern class Switch extends ReactComponentOfProps<SwitchProps> {}
