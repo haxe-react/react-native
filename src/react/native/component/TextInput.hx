31a1332b91c37ccfa2854ac64012c332b072ef49
Kevin Leung <kevinresol@gmail.com>
2017-06-05 17:24:25 +0800

    More props

--- src/react/native/component/TextInput.hx
+++ src/react/native/component/TextInput.hx
@@ -1,4 +1,7 @@
 package react.native.component;
 
+import react.native.component.props.*;
+import react.ReactComponent;
+
 @:jsRequire('react-native', 'TextInput')
-extern class TextInput extends react.ReactComponent {}
+extern class TextInput extends ReactComponentOfProps<TextInputProps> {}
--- src/react/native/component/TouchableOpacity.hx
+++ src/react/native/component/TouchableOpacity.hx
@@ -1,4 +1,7 @@
 package react.native.component;
 
+import react.native.component.props.*;
+import react.ReactComponent;
+
 @:jsRequire('react-native', 'TouchableOpacity')
-extern class TouchableOpacity extends react.ReactComponent {}
+extern class TouchableOpacity extends ReactComponentOfProps<TouchableOpacityProps> {}
