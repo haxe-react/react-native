391ba5d5627a5b8401e4e9f3007bd6ae419bca03
Kevin Leung <kevinresol@gmail.com>
2017-12-07 13:29:14 +0800

    Update api

--- src/react/native/api/AlertIOS.hx
+++ src/react/native/api/AlertIOS.hx
@@ -6,5 +6,5 @@ import haxe.Constraints;
 @:jsRequire('react-native', 'AlertIOS')
 extern class AlertIOS {
 	static function alert(title:String, ?message:String, ?buttons:EitherType<Void->Void, Array<{text:String, ?onPress:Void->Void, ?style:String}>>):Void;
-	static function prompt(title:String, ?message:String, ?buttons:EitherType<String->Void, Array<{text:String, ?onPress:String->Void, ?style:String}>>):Void;
+	static function prompt(title:String, ?message:String, ?buttons:EitherType<String->Void, Array<{text:String, ?onPress:String->Void, ?style:String}>>, type:String, defaultValue:String, keyboardType:String):Void;
 }
