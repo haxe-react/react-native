12a4c079ae485121f1361ab7ffe7851c5eb09815
Kevin Leung <kevinresol@gmail.com>
2017-12-07 13:12:47 +0800

    Add AlertIOS

---
+++ src/react/native/api/AlertIOS.hx
@@ -0,0 +1,10 @@
+package react.native.api;
+
+import haxe.extern.EitherType;
+import haxe.Constraints;
+
+@:jsRequire('react-native', 'AlertIOS')
+extern class AlertIOS {
+	static function alert(title:String, ?message:String, ?buttons:EitherType<Void->Void, Array<{text:String, ?onPress:Void->Void, ?style:String}>>):Void;
+	static function prompt(title:String, ?message:String, ?buttons:EitherType<String->Void, Array<{text:String, ?onPress:String->Void, ?style:String}>>):Void;
+}
