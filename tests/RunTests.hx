a4b58ba2ff19b0c3e098cde769636cb036c3fead
Kevin Leung <kevinresol@gmail.com>
2018-04-20 11:31:37 +0800

    WIP typed stylesheet https://github.com/HaxeFoundation/haxe/issues/6961

--- tests/RunTests.hx
+++ tests/RunTests.hx
@@ -10,12 +10,19 @@ class RunTests {
       container: @:type(ViewStyle) {
         alignItems: Center,
       },
-      view: {
-        justifyContent: 'center',
+      view:  @:type(ViewStyle) {
+        justifyContent: Center,
       },
     });
     
-    $type(style.view);
+    // $type(style.view);
+    
+    var props:ViewProps = {style: style.view}
+    var props:ViewProps = {style: [style.container, style.view]}
+    var props:ViewProps = {style: [style.container, {justifyContent: Center}]}
+    var props:ViewProps = {style: [{justifyContent: Center}]}
+    // var props:ViewProps = {style: {justifyContent: Center}}
+    var props:ViewProps = {style: {justifyContent: cast 'center'}}
     
     travix.Logger.println('it works');
     travix.Logger.exit(0); // make sure we exit properly, which is necessary on some targets, e.g. flash & (phantom)js
