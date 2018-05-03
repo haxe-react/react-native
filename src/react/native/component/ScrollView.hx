fc4e23bccf40f617f1648d69a624b82ea0640107
Kevin Leung <kevinresol@gmail.com>
2018-05-03 21:50:24 +0800

    Add scrollToEnd

--- src/react/native/component/ScrollView.hx
+++ src/react/native/component/ScrollView.hx
@@ -6,6 +6,7 @@ import react.ReactComponent;
 @:jsRequire('react-native', 'ScrollView')
 extern class ScrollView extends ReactComponentOfProps<ScrollViewProps> {
 	function scrollTo(options:{?x:Float, ?y:Float, ?animated:Bool}):Void;
+	function scrollToEnd(options:{?animated:Bool}):Void;
 	function getScrollResponder():ScrollResponder;
 }
 
