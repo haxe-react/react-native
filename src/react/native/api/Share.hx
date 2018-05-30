11863b7cbbd4071f42a87fb7a388ab026417aa4f
Kevin Leung <kevinresol@gmail.com>
2018-05-31 00:50:20 +0800

    Type Share Api

--- src/react/native/api/Share.hx
+++ src/react/native/api/Share.hx
@@ -5,7 +5,14 @@ import js.Promise;
 
 @:jsRequire("react-native", "Share")
 extern class Share {
-	static function share(content:Content, ?options:Options):Promise<Dynamic>;
+	static var sharedAction:String;
+	static var dismissedAction:String;
+	static function share(content:Content, ?options:Options):Promise<Result>;
+}
+
+private typedef Result = {
+	action:String,
+	?activityType:String,
 }
  
 private typedef Content = {
