907fd6bd0137f600bd9a2d14666e8db413b1f889
Kevin Leung <kevinresol@gmail.com>
2019-02-01 12:00:25 +0800

    More permissive about event listener signatures

--- src/react/native/api/DeviceEventEmitter.hx
+++ src/react/native/api/DeviceEventEmitter.hx
@@ -1,8 +1,10 @@
 package react.native.api;
 
+import haxe.Constraints;
+
 @:jsRequire('react-native', 'DeviceEventEmitter')
 extern class DeviceEventEmitter {
-    static public function addListener(event:String,cb:Dynamic->Void,?context:Dynamic):EmitterSubscription;
+    static public function addListener(event:String, cb:Function, ?context:Dynamic):EmitterSubscription;
     static public function removeAllListeners(eventType:String):Void;
     static public function removeSubscription(sub:EmitterSubscription):Void;
 }
