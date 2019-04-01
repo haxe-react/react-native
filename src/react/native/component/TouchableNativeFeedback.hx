dbd92e894b98dc497119a29676063cc8cf958849
Kevin Leung <kevinresol@gmail.com>
2019-04-01 22:37:15 +0800

    Add functions to TouchableNativeFeedback

--- src/react/native/component/TouchableNativeFeedback.hx
+++ src/react/native/component/TouchableNativeFeedback.hx
@@ -4,4 +4,8 @@ import react.native.component.props.*;
 import react.ReactComponent;
 
 @:jsRequire('react-native', 'TouchableNativeFeedback')
-extern class TouchableNativeFeedback extends ReactComponentOfProps<TouchableNativeFeedbackProps> {}
+extern class TouchableNativeFeedback extends ReactComponentOfProps<TouchableNativeFeedbackProps> {
+	static function SelectableBackground():Dynamic;
+	static function SelectableBackgroundBorderless():Dynamic;
+	static function Ripple(color:String, borderless:Bool):Dynamic;
+}
