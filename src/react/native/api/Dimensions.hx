dca4e384895a3de0c8ed3a9b87f086c8fa7877a0
Kevin Leung <kevinresol@gmail.com>
2019-04-02 00:29:30 +0800

    Type Dimensions.get() (closes #4)

--- src/react/native/api/Dimensions.hx
+++ src/react/native/api/Dimensions.hx
@@ -2,5 +2,11 @@ package react.native.api;
 
 @:jsRequire('react-native', 'Dimensions')
 extern class Dimensions {
-	static function get(dim:String):{width:Int, height:Int};
+	static function get(dim:DimensionTarget):{width:Int, height:Int};
+}
+
+
+@:enum abstract DimensionTarget(String) from String {
+	var Window = 'window';
+	var Screen = 'screen';
 }
