445ac285f2298595aae6fbfd89d4a7c897e330db
serjek <sbalitskiy@gmail.com>
2019-10-22 10:15:12 +0200

    Update ImageBackground.hx
    extended with imageStyle prop

--- src/react/native/component/ImageBackground.hx
+++ src/react/native/component/ImageBackground.hx
@@ -3,5 +3,9 @@ package react.native.component;
 import react.native.component.props.*;
 import react.ReactComponent;
 
+//ImageBackground uses both style for self and imageStyle for underlaying Image component
 @:jsRequire('react-native', 'ImageBackground')
-extern class ImageBackground extends ReactComponentOfProps<ImageProps> {}
+extern class ImageBackground extends ReactComponentOfProps<{
+  >ImageProps,
+  ?imageStyle:haxe.extern.EitherType<ImageStyle, Array<ImageStyle>>
+}> {}
